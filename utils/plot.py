25c39beecf8625a0b796e3bdfdf923041f719a04
Diego Armando Juarez <dajuarez@login31.chn.perlmutter.nersc.gov>
2025-05-14 22:43:46 -0700

    solving a small error

--- utils/plot.py
+++ utils/plot.py
@@ -5,8 +5,10 @@ import pandas as pd
 plt.figure(figsize=(15, 10), dpi=50)
 
 def plot_mech_prop(Aluminium_concentration, parameter,  parameter_error, linestyles_plot, colors_plot, markers_plot,label_plot, defect,temperature_variation ):
+    
     plt.errorbar(Aluminium_concentration[0:len(reduction_percentages)], parameter, yerr=parameter_error, 
-                     fmt=markers_plot[defect], linestyle=linestyles_plot[defect], color=colors_plot[temperature_variation],  
+                     fmt=markers_plot[defect_percentages.index(defect)], linestyle=linestyles_plot[defect_percentages.index(defect)], color=colors_plot[temperature_variation],  
                      markersize=22, markerfacecolor='none', linewidth=2.5, label=label_plot) 
+    
 
 
